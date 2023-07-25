9b8801d9c55a7887503273c1ebd836780b726336
KaffinPX <kaffinpx@gmail.com>
2023-07-25 04:06:21 +0300

    some refactorization

--- index.js
+++ index.js
@@ -8,12 +8,11 @@ const Logger = require('./src/services/monitoring')
 
 const config = require('./config.json')
 
-const database = new Database(config.services.database)
-
 const kaspa = new Kaspa(config.kaspa.nodeAddress, () => {
   const wallet = new Wallet(kaspa.networkType, config.kaspa.wallet.mnemonics, () => {
+    const database = new Database(config.database)
     const pool = new Pool(kaspa, wallet, database, config.pool)
-    
+
     new RPC(config.services.RPC.port, pool)
     new Logger()
   })
